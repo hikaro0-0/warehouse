db58cf4323161ad9c2735875bc06aea77d2bcc4f
Жилинский Олег <olegzilinskij4@gmail.com>
2026-03-09 09:49:38 +0300

    delete duplications

--- src/main/java/com/hikaro/warehouse/dto/ProductResponseDto.java
+++ src/main/java/com/hikaro/warehouse/dto/ProductResponseDto.java
@@ -1,51 +1,9 @@
 package com.hikaro.warehouse.dto;
 
-public class ProductResponseDto {
-
-    private Long id;
-    private String name;
-    private Integer quantity;
-    private String location;
-
-    public ProductResponseDto() {
-    }
-
-    public ProductResponseDto(Long id, String name, Integer quantity, String location) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-        this.location = location;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
+public record ProductResponseDto(
+        Long id,
+        String name,
+        Integer quantity,
+        String location
+) {
 }
